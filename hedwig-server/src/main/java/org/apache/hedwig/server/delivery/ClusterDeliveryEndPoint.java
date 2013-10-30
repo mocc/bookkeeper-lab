fd554fe380ead699c514223f16c315a19dc36d0a
huangruiqing <ruoyiqing1990@163.com>
2013-10-30 17:39:52 +0800

    revise clusterDeliveryEndPoint.java in messageConsumed method.

--- hedwig-server/src/main/java/org/apache/hedwig/server/delivery/ClusterDeliveryEndPoint.java
+++ hedwig-server/src/main/java/org/apache/hedwig/server/delivery/ClusterDeliveryEndPoint.java
@@ -235,10 +235,12 @@ public class ClusterDeliveryEndPoint implements DeliveryEndPoint, ThrottlingPoli
             if (state != null) {
                 state.msgs.remove(newSeqIdConsumed);
             }
-            if (throttledEP.contains(lastDeliveredEP)) {
-                throttledEP.remove(lastDeliveredEP);
-                deliverableEP.offer(lastDeliveredEP);
-                return true;
+            synchronized (deliverableEP) {
+                if (throttledEP.contains(lastDeliveredEP)) {
+                    throttledEP.remove(lastDeliveredEP);
+                    deliverableEP.offer(lastDeliveredEP);
+                    return true;
+                }
             }
         }
         return false;
